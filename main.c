86440c2f934db471be417a3214da624352146cf7
DeshonS <deshonjewell@gmail.com>
2024-08-05 17:05:02 -0500

    re-added main.c

---
+++ main.c
@@ -0,0 +1,43 @@
+#include "monty.h"
+
+int main(void)
+{
+    stack_t *stack = NULL;
+    char line[256];
+    unsigned int line_number = 0;
+
+    while (fgets(line, sizeof(line), stdin))
+    {
+        line_number++;
+        char *opcode = strtok(line, " \t\r\n");
+
+        if (opcode == NULL)
+        {
+            continue;
+        }
+
+        if (strcmp(opcode, "push") == 0)
+        {
+            push(&stack, line_number);
+        }
+        else if (strcmp(opcode, "pall") == 0)
+        {
+            pall(&stack);
+        }
+        else
+        {
+            fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Free the stack
+    while (stack != NULL)
+    {
+        stack_t *temp = stack;
+        stack = stack->next;
+        free(temp);
+    }
+
+    return 0;
+}
